4f72763ba86bea5cf8d2a846e5c32f745049bbf0
zakiezabar <zakiezabar@gmail.com>
2025-06-20 01:40:51 +0800

    updated config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Keep TypeScript checking enabled
+    ignoreBuildErrors: false,
+  },
 };
 
 export default nextConfig;
